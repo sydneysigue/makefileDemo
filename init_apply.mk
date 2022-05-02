786401226566c5a15d219024d3ea62c57d11bc9c
sydneysigue <32209372+sydneysigue@users.noreply.github.com>
2022-05-02 14:29:02 -0400

    Update init_apply.mk

--- init_apply.mk
+++ init_apply.mk
@@ -6,7 +6,6 @@ MIN_SIZE ?= 1
 MAX_SIZE ?= 1
 
 flow:
-	@terraform init
 	@terraform plan -var cluster_name=${CLUSTER_NAME} -var instance_type=${INSTANCE_TYPE} -var max_size=${MAX_SIZE} -var min_size=${MIN_SIZE} -var region=${REGION}
 	
 
